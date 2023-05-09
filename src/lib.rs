188064872cfcfb06b61416ac34ebf3bf22554921
Carl Lundin <carllundin55@gmail.com>
2023-05-09 16:56:45 -0700

    feat: Added dry-run.

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,54 @@ use std::fs::rename;
 use std::path::{Path, PathBuf};
 use walkdir::{DirEntry, WalkDir};
 
+#[derive(PartialEq)]
+pub enum Operation {
+    Mutating,
+    DryRun,
+}
+
+pub struct FileRename(String, String);
+
+impl FileRename {
+    pub fn new(original_name: String, updated_name: String) -> Self {
+        Self(original_name, updated_name)
+    }
+    pub fn get_original_name(&self) -> &str {
+        &self.0
+    }
+
+    pub fn get_updated_name(&self) -> &str {
+        &self.1
+    }
+}
+
 pub type RenameError = Box<dyn std::error::Error>;
 
 pub fn bulk_rename(
     root_dir: &Path,
-    existing_name: &str,
-    new_name: &str,
-) -> Result<Vec<String>, RenameError> {
-    let matches = find_matching_entries(existing_name, root_dir)?;
-    let operations = matches
+    original: &str,
+    update: &str,
+    operation: &Operation,
+) -> Result<Vec<FileRename>, RenameError> {
+    let matches = find_matching_entries(original, root_dir)?;
+    let operations: Vec<FileRename> = matches
         .iter()
-        .filter_map(|m| {
-            if let Some(new_name) = rename_file(m, existing_name, new_name) {
-                return Some(new_name);
-            }
-            None
-        })
+        .filter_map(|m| file_rename(m, original, update))
         .collect();
+
+    for names in &operations {
+        let res = match operation {
+            Operation::Mutating => rename(names.get_original_name(), names.get_updated_name()),
+            Operation::DryRun => Ok(()),
+        };
+        if let Err(e) = res {
+            eprintln!(
+                "Failed to rename {} due to {}",
+                names.get_original_name(),
+                e
+            );
+        }
+    }
 
     Ok(operations)
 }
@@ -30,25 +61,22 @@ fn find_matching_entries(
     let w = WalkDir::new(root_dir);
 
     let matching_files = w.into_iter().filter_map(|e| match e {
-        Ok(entry) if is_match(existing_name, &entry) => Some(entry.into_path()),
+        Ok(entry) if matches_existing_name(existing_name, &entry) => Some(entry.into_path()),
         _ => None,
     });
 
     Ok(matching_files.collect())
 }
 
-fn rename_file(file: &Path, existing_name: &str, new_name: &str) -> Option<String> {
+fn file_rename(file: &Path, original_name: &str, new_name: &str) -> Option<FileRename> {
     if let Some(file_name) = file.to_str() {
-        let new_name = file_name.replace(existing_name, new_name);
-
-        if rename(file_name, &new_name).is_ok() {
-            return Some(new_name);
-        }
+        let new_file_name = file_name.replace(original_name, new_name);
+        return Some(FileRename::new(file_name.to_owned(), new_file_name));
     }
     None
 }
 
-fn is_match(target: &str, entry: &DirEntry) -> bool {
+fn matches_existing_name(target: &str, entry: &DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,48 @@ struct Args {
     #[arg(short, long)]
     root_dir: String,
 
-    /// Existing pattern
+    /// Original pattern
     #[arg(short, long)]
-    existing: String,
+    original: String,
 
-    /// New pattern
+    /// Updated pattern
     #[arg(short, long)]
-    new_pattern: String,
+    update: String,
+
+    /// Dry-run. Enable to avoid mutating file system.
+    #[arg(short, long)]
+    dry_run: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let p = Path::new(&args.root_dir);
 
-    let operations = bulk_renamer::bulk_rename(p, &args.existing, &args.new_pattern)?;
-    for new_name in operations {
-        println!("Renamed to {new_name}");
+    let operation = if args.dry_run {
+        bulk_renamer::Operation::DryRun
+    } else {
+        bulk_renamer::Operation::Mutating
+    };
+
+    let operations = bulk_renamer::bulk_rename(p, &args.original, &args.update, &operation)?;
+
+    for names in operations {
+        match operation {
+            bulk_renamer::Operation::DryRun => {
+                println!(
+                    "{} would have been renamed to {}.",
+                    names.get_original_name(),
+                    names.get_updated_name()
+                );
+            }
+            bulk_renamer::Operation::Mutating => {
+                println!(
+                    "Renamed {} to {}",
+                    names.get_original_name(),
+                    names.get_updated_name()
+                );
+            }
+        }
     }
 
     Ok(())
